86a3c1b225b20f0688ee7c11545d9f1b34905738
Shubham Salunke <shubhamdeepak007@gmail.com>
2023-02-11 05:45:58 +0530

    stream api

--- src/screens/Stream.jsx
+++ src/screens/Stream.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DroneList from "../components/HomeComponents/DroneList";
 import axios from "axios";
+import OT, { Publisher } from '@opentok/client'
 
 const Stream = () => {
   const [SelectedDroneId, setSelectedDroneId] = useState(1);
@@ -10,6 +11,9 @@ const Stream = () => {
   const [SelectedDrone, setSelectedDrone] = useState();
   const [loader, setloader] = useState(true);
   const [DroneData, setDroneData] = useState();
+  const [data, setData] = useState();
+
+  const videoRef = useRef(null);
 
   useEffect(() => {
     const getDroneData = async () => {
@@ -18,18 +22,81 @@ const Stream = () => {
       setDroneData(res.data);
 
       console.log(res.data)
-    };
+
+
+
+      // Shubham Coded from Here:
+      // await axios.get('http://127.0.0.1:5000/gen_token').then(res => {
+      //   setStreamKeys(res.data);
+      // }).catch(err => {
+      //   console.log(err)
+      // })
+
+      // navigator.mediaDevices.getUserMedia({video: true}).then(stream => setSrc(stream))
+
+    //   let session = OT.initSession(apiKey, streamKeys.session);
+    //   session.on('streamCreated', event => {
+    //     session.subscribe(event.stream, 'subscriber', {
+    //       insertMode: 'append',
+    //       width: '100%',
+    //       height: '100%'
+    //     }, err => console.log(err))
+
+    //   let publisher = OT.initPublisher('publisher', {
+    //     insertMode: 'append',
+    //     width: '100%',
+    //     height: '100%'
+    //   }, err => console.log(err));
+
+    //     session.connect(streamKeys.data, err => {
+    //       err ? console.log(err) : session.publish(publisher, err => console.log(err))
+    //     })
+    //   })
+    // };
+
+    }
 
     getDroneData();
+
+    // const ssc = new EventSource('http://localhost:5000/video')
+    // function handleStream(e) {
+    //   console.log(e);
+    //   setData(e.data);
+    // }
+    // ssc.onmessage = e => {
+    //   handleStream(e);
+    // }
+    // ssc.onerror = e => {
+    //   ssc.close();
+    // }
+
+    const video = videoRef.current
+
+    const updateVideo = () => {
+      video.src = 'http://localhost:5000/video';
+      console.log(video.src);
+      video.addEventListener('loadmetdata', () => {
+        video.play();
+      })
+    }
+
+    updateVideo();
   }, []);
 
+
+
   return (
+    <>
     <div className="p-12 pt-20 ">
       <h1 className="text-3xl font bold text-lightpeach">Stream</h1>
       <div className=" mt-4">
         <DroneList data={DroneData} setSelectedDroneId={setSelectedDroneId} />
       </div>
+      
+        
     </div>
+    <video autoPlay ref={videoRef}></video>
+    </> 
   );
 };
 
